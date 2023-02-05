8779e594da03026a8ce00fbb943980622b1ce849
Vishal Brijesh kanojiya <90042549+kanojiya9768@users.noreply.github.com>
2023-02-05 11:40:24 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -335,3 +335,75 @@ var bar = new ProgressBar.Circle("#mongo", {
   bar.text.style.fontSize = '2rem';
   
   bar.animate(0.72);  // Number from 0.0 to 1.0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+//html5 skill bar 
+var bar = new ProgressBar.Circle("#excel", {
+  color: '#aaa',
+  // This has to be the same size as the maximum width to
+  // prevent clipping
+  strokeWidth: 4,
+  trailWidth: 1,
+  easing: 'easeInOut',
+  duration: 1400,
+  text: {
+    autoStyleContainer: false
+  },
+  from: { color: '#72e2ae', width: 5 },
+  to: { color: '#72e2ae', width: 5
+},
+  // Set default step function for all animate calls
+  step: function(state, circle) {
+    circle.path.setAttribute('stroke', state.color);
+    circle.path.setAttribute('stroke-width', state.width);
+
+    var value = Math.round(circle.value() * 100);
+    if (value === 0) {
+      circle.setText('');
+    } else {
+      circle.setText(value);
+    }
+
+  }
+});
+bar.text.style.fontFamily = '"Raleway", Helvetica, sans-serif';
+bar.text.style.fontSize = '2rem';
+
+bar.animate(0.51);  // Number from 0.0 to 1.0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
